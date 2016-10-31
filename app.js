c190fcbf91f0f99ff2661ad12705867e9b16ec29
Niall O'Brien <me@niallobrien.me>
2016-10-31 14:20:11 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,145 @@
+/**
+ * Module dependencies.
+ */
+const express = require('express');
+const manifest = require('express-rev');
+const compression = require('compression');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const chalk = require('chalk');
+const errorHandler = require('errorhandler');
+const lusca = require('lusca');
+const dotenv = require('dotenv');
+const MongoStore = require('connect-mongo')(session);
+const flash = require('express-flash');
+const path = require('path');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const expressValidator = require('express-validator');
+const expressStatusMonitor = require('express-status-monitor');
+const sass = require('node-sass-middleware');
+const multer = require('multer');
+const upload = multer({ dest: path.join(__dirname, 'uploads') });
+
+/**
+ * Load environment variables from .env file, where API keys and passwords are configured.
+ */
+dotenv.load({ path: '.env.example' });
+
+
+/**
+ * API keys and Passport configuration.
+ */
+const passportConfig = require('./config/passport');
+
+/**
+ * Create Express & Socket.io servers.
+ */
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+/**
+ * Connect to MongoDB.
+ */
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
+mongoose.connection.on('connected', () => {
+  console.log('%s MongoDB connection established!', chalk.green('✓'));
+});
+mongoose.connection.on('error', () => {
+  console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
+  process.exit();
+});
+
+/**
+ * Express configuration.
+ */
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(manifest({
+  manifest: './public/js/manifest.json',
+  prepend: '/js'
+}));
+app.use(expressStatusMonitor());
+app.use(compression());
+app.use(sass({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public')
+}));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressValidator());
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: process.env.SESSION_SECRET,
+  store: new MongoStore({
+    url: process.env.MONGODB_URI || process.env.MONGOLAB_URI,
+    autoReconnect: true
+  })
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use((req, res, next) => {
+  if (req.path === '/api/upload') {
+    next();
+  } else {
+    lusca.csrf()(req, res, next);
+  }
+});
+app.use(lusca.xframe('SAMEORIGIN'));
+app.use(lusca.xssProtection(true));
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
+});
+app.use(function(req, res, next) {
+  // After successful login, redirect back to the intended page
+  if (!req.user &&
+      req.path !== '/login' &&
+      req.path !== '/signup' &&
+      !req.path.match(/^\/auth/) &&
+      !req.path.match(/\./)) {
+    req.session.returnTo = req.path;
+  }
+  next();
+});
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
+
+/**
+ * Primary app routes.
+ */
+require('./config/routes')(app, passport, passportConfig, upload);
+
+/**
+ * Error Handler.
+ */
+app.use(errorHandler());
+
+/**
+ * Socket.io.
+ */
+io.on('connection', function(socket) {
+  socket.emit('greet', { hello: 'Hey there browser!' });
+  socket.on('respond', function(data) {
+    console.log(data);
+  });
+  socket.on('disconnect', function() {
+    console.log('Socket disconnected');
+  });
+});
+
+/**
+ * Start Express server.
+ */
+server.listen(app.get('port'), () => {
+  const host = (app.get('env') === 'development') ? 'http://localhost:%d' : 'port %d';
+  console.log('%s Express server listening on ' + host + ' in %s mode.', chalk.green('✓'), app.get('port'), app.get('env'));
+});
+
+module.exports = app;
---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+// If you want to move Bootstrap from the public directory & integrate with Webpack:
+// https://github.com/AngularClass/angular2-webpack-starter/issues/696#issuecomment-227786637
+var webpack = require('webpack');
+var ManifestPlugin = require('webpack-manifest-plugin');
+var ChunkManifestPlugin = require('chunk-manifest-webpack-plugin');
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+var path = require('path');
+
+var nodeModulesPath = path.resolve(__dirname, 'node_modules');
+var buildPath = path.resolve(__dirname, 'public', 'js');
+var mainPath = path.resolve(__dirname, 'assets', 'scripts', 'main.js');
+var config = {
+  devtool: 'eval',
+  entry: {
+    vendor: ['jquery', 'simple-pjax'],
+    main: mainPath
+  },
+  output: {
+    path: buildPath,
+    filename: '[name].js',
+    publicPath: 'js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: nodeModulesPath,
+        query: {
+          plugins: ['transform-runtime'],
+          presets: ['es2015']
+        }
+       },
+      { test: /vendor\/.+\.(jsx|js)$/, loader: 'imports?jQuery=jquery,$=jquery,this=>window' },
+      {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/font-woff&name=fonts/[name].[ext]'
+      },
+      {
+        test: /\.woff2(\?\S*)?$/,
+        loader: 'url?limit=10000&mimetype=application/font-woff2&name=fonts/[name].[ext]'
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/font-tff&name=fonts/[name].[ext]'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file?name=fonts/[name].[ext]'
+      },
+      {
+        test: /\.png(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file?name=img/[name].[ext]'
+      },
+      {
+        test: /\.gif(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file?name=img/[name].[ext]'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=image/svg+xml&name=img/[name].[ext]'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery'
+    }),
+    new webpack.optimize.CommonsChunkPlugin('vendor', '[name].js'),
+    new ManifestPlugin(),
+    new ChunkManifestPlugin({
+      filename: 'manifest.json',
+      manifestVariable: 'webpackManifest'
+    }),
+    // remove if you don't want Browsersync
+    new BrowserSyncPlugin(
+      {
+        // Browse to http://localhost:3030/ during development
+        host: 'localhost',
+        port: 3030,
+        // Assumes server is running on port 3000
+        proxy: 'http://localhost:3000/'
+      },
+      { reload: true }
+    )
+
+  ]
+};
+
+module.exports = config;
