b3e5aa0839500069fb57144e4dbbbe75852ea847
Niall O'Brien <me@niallobrien.me>
2017-01-10 17:16:29 +0000

    Updates configs to Webpack v2 with code-splitting etc.

--- webpack.config.js
+++ webpack.config.js
@@ -1,94 +1,44 @@
-// If you want to move Bootstrap from the public directory & integrate with Webpack:
-// https://github.com/AngularClass/angular2-webpack-starter/issues/696#issuecomment-227786637
-const webpack = require('webpack')
-const ManifestPlugin = require('webpack-manifest-plugin')
-const ChunkManifestPlugin = require('chunk-manifest-webpack-plugin')
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
-const path = require('path')
-
-const nodeModulesPath = path.resolve(__dirname, 'node_modules')
+const webpack = require('webpack');
+const ManifestPlugin = require('webpack-manifest-plugin');
+const path = require('path');
+const srcPath = path.resolve(__dirname, 'assets', 'scripts', 'main.js')
 const buildPath = path.resolve(__dirname, 'public', 'js')
-const mainPath = path.resolve(__dirname, 'assets', 'scripts', 'main.js')
+const nodeModulesPath = path.resolve(__dirname, 'node_modules')
+
 const config = {
-  devtool: 'eval',
+  devtool: 'source-map',
   entry: {
-    vendor: ['jquery', 'bootstrap-sass', 'chart.js', 'simple-pjax'],
-    main: mainPath
+    vendor: ['jquery', 'bootstrap-sass', 'simple-pjax', 'chart.js'],
+    main: srcPath
   },
   output: {
     path: buildPath,
     filename: '[name].js',
     publicPath: 'js'
   },
+
   module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel',
-        exclude: nodeModulesPath,
-        query: {
-          plugins: ['transform-runtime'],
-          presets: ['es2015']
-        }
-      },
-      {
-        test: /vendor\/.+\.(jsx|js)$/,
-        loader: 'imports?jQuery=jquery,$=jquery,this=>window'
-      },
-      {
-        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/font-woff&name=fonts/[name].[ext]'
-      },
-      {
-        test: /\.woff2(\?\S*)?$/,
-        loader: 'url?limit=10000&mimetype=application/font-woff2&name=fonts/[name].[ext]'
-      },
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/font-tff&name=fonts/[name].[ext]'
-      },
-      {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file?name=fonts/[name].[ext]'
-      },
-      {
-        test: /\.png(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file?name=img/[name].[ext]'
-      },
-      {
-        test: /\.gif(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file?name=img/[name].[ext]'
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=image/svg+xml&name=img/[name].[ext]'
-      }
-    ]
+    rules: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: nodeModulesPath,
+      options: { presets: ['es2015'] },
+    }]
   },
+
   plugins: [
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery',
       'window.jQuery': 'jquery'
     }),
-    new webpack.optimize.CommonsChunkPlugin('vendor', '[name].js'),
-    new ManifestPlugin(),
-    new ChunkManifestPlugin({
-      filename: 'manifest.json',
-      manifestVariable: 'webpackManifest'
+    // Abstract a common file between all config.entry JS
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'commons.chunk',
+      filename: 'commons.chunk.js'
     }),
-    // remove if you don't want Browsersync
-    new BrowserSyncPlugin(
-      {
-        // Browse to http://localhost:3030/ during development
-        host: 'localhost',
-        port: 3030,
-        // Assumes server is running on port 3000
-        proxy: 'http://localhost:3000/'
-      },
-      { reload: true }
-    )
+    new ManifestPlugin()
   ]
-}
+};
 
-module.exports = config
+module.exports = config;
