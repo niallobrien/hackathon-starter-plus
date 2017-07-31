12cf8f4959af3af5e1786ff704e3375219b3843f
Niall O'Brien <me@niallobrien.me>
2017-07-31 10:09:46 +0100

    Integrates latest upstream changes

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const expressValidator = require('express-validator');
 const expressStatusMonitor = require('express-status-monitor');
-const sass = require('node-sass-middleware');
 const multer = require('multer');
 const upload = multer({ dest: path.join(__dirname, 'uploads') });
 
